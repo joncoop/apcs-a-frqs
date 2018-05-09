f128d3888bf966d4cfbbeccffe5f1979ddfb7a7f
Jon Cooper <joncoop@users.noreply.github.com>
2018-05-09 12:07:52 -0400

    Update Customer.java

--- 2006/3. Customer/Customer.java
+++ 2006/3. Customer/Customer.java
@@ -1,21 +1,33 @@
 
 public class Customer {
 
-	String name;
-	int idNum;
+	private String name;
+	private int idNum;
 	
 	public Customer(String name, int idNum) {
 		this.name = name;
 		this.idNum = idNum;
 	}
 	
+	public String getName() {
+		return name;	
+	}
+	
+	public int getID() {
+		return idNum;	
+	}
+	
 	public int compareCustomer(Customer other) {
 		
 		if (this.name.equals(other.name)) {
-			return this.idNum - other.idNum;
+			return this.getID() - other.getID();
+			// Actual FRQ requires getters to access fields since quiestion doesn't give field names.
+			// Otherwise we could just use the following:
+			// return this.idNum - other.idNum;
 		}
 		else {
-			return this.name.compareTo(other.name);
+			return this.getName().compareTo(other.getName());
+			// return this.name.compareTo(other.name);
 		}
 	}
 
