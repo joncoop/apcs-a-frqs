58a470c0ba4f222014d197f2e938e97f941966c4
Jon Cooper <joncoop@users.noreply.github.com>
2018-11-02 11:10:22 -0400

    Update ChargerTester.java

--- 2009/3. Battery Charger/ChargerTester.java
+++ 2009/3. Battery Charger/ChargerTester.java
@@ -3,15 +3,25 @@ public class ChargerTester {
 
 	public static void main(String[] args) {
 
-		int[] rates = {1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4,
-				       4, 4, 4, 3, 3, 3, 2, 2, 2, 1, 1, 1};
-		
-		System.out.println(rates.length);
+		int[] rates = { 50,  60, 160,  60,  80, 100, 100, 120,
+			       150, 150, 150, 200,  40, 240, 220, 220,
+			       200, 200, 180, 180, 140, 100,  80,  60};
 		
 		BatteryCharger charger = new BatteryCharger(rates);
 		
-		System.out.println( charger.getChargingCost(20, 9) );
-		System.out.println( charger.getChargeStartTime(8) );
+		// temporarily make getChargingCost public to test this method
+		/*
+		System.out.println( charger.getChargingCost(12, 1) ); 
+		System.out.println( charger.getChargingCost(0, 2) ); 
+		System.out.println( charger.getChargingCost(22, 7) ); 
+		System.out.println( charger.getChargingCost(22, 30) ); 
+		*/
+		
+		// test getChargeStartTime
+		System.out.println( charger.getChargeStartTime(1) );
+		System.out.println( charger.getChargeStartTime(2) );
+		System.out.println( charger.getChargeStartTime(7) );
+		System.out.println( charger.getChargeStartTime(30) );
 	}
 
 }
