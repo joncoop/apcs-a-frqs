6b115ab47aae7ca4d74c62e0db325b2aba3defab
Jon Cooper <joncoop@users.noreply.github.com>
2021-05-19 10:58:27 -0400

    Update ClubMembers.java

--- 2021/3. Club Members/ClubMembers.java
+++ 2021/3. Club Members/ClubMembers.java
@@ -2,38 +2,38 @@ import java.util.*;
 
 public class ClubMembers
 {
-	private ArrayList<MemberInfo> memberList;
+    private ArrayList<MemberInfo> memberList;
 
-   public ClubMembers( )
-   {
-   	//stuff
-   }
+    public ClubMembers( )
+    {
+        //stuff
+    }
 
-   public void addMembers(String[] names, int gradYear)
-   {
-   	for (String n: names) {
-         MemberInfo info = new MemberInfo(n, gradYear, true);
-         memberList.add(info);
-      }
-   }
-
-   public ArrayList<MemberInfo> removeMembers(int year)
-   {
-   	ArrayList<MemberInfo> result = new ArrayList<MemberInfo>();
-
-      for (int i = memberList.size(); i >= 0; i--) {
-         MemberInfo info = memberList.get(i);
-
-         if (info.getGradYear() <= year) {
-            if (info.inGoodStanding()) {
-               result.add(info);
-            }
-
-            memberList.remove(i);
+    public void addMembers(String[] names, int gradYear)
+    {
+   	 for (String n: names) {
+              MemberInfo info = new MemberInfo(n, gradYear, true);
+              memberList.add(info);
          }
-      }
+    }
 
-      return result;
-   }
+    public ArrayList<MemberInfo> removeMembers(int year)
+    {
+        ArrayList<MemberInfo> result = new ArrayList<MemberInfo>();
+
+        for (int i = memberList.size(); i >= 0; i--) {
+            MemberInfo info = memberList.get(i);
+
+            if (info.getGradYear() <= year) {
+                if (info.inGoodStanding()) {
+                    result.add(info);
+                }
+
+                memberList.remove(i);
+            }
+        }
+
+        return result;
+    }
 
 }
