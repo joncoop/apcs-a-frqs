b6ff3b240fbb8fe091da8d05bb9e2a6277c5ed7d
Jon Cooper <joncoop@users.noreply.github.com>
2022-03-16 15:42:59 -0400

    Update WordMatch.java

--- 2021/1. Word Match/WordMatch.java
+++ 2021/1. Word Match/WordMatch.java
@@ -1,46 +1,45 @@
 public class WordMatch
 {
-	private String secret;
+    private String secret;
 
-	public WordMatch( String word )
-	{
-		this.secret = word;
-	}
+    public WordMatch( String word )
+    {
+        this.secret = word;
+    }
 
-   public int scoreGuess(String guess)
-   {
-   	int count = 0;
-      int gLen = guess.length();
-      int sLen = secret.length();
+    public int scoreGuess(String guess)
+    {
+        int count = 0;
+        int gLen = guess.length();
+        int sLen = secret.length();
 
-      for (int i = 0; i < sLen - gLen; i++) {
-         String chunk = secret.substring(i, i + gLen);
+        for (int i = 0; i < sLen - gLen; i++) {
+            String chunk = secret.substring(i, i + gLen);
 
-         if (guess.equals(chunk)) {
-            count++;
-         }
-      }
+            if (guess.equals(chunk)) {
+                count++;
+             }
+        }
 
-      return count * gLen * gLen;
+        return count * gLen * gLen;
    }
 
    public String findBetterGuess( String guess1, String guess2 )
    {
-   	int s1 = scoreGuess(guess1);
-      int s2 = scoreGuess(guess2);
+	int s1 = scoreGuess(guess1);
+	int s2 = scoreGuess(guess2);
 
-      if (s1 > s2) {
-         return guess1;
-      }
-      else if (s2 > s1) {
-         return guess2;
-      }
-      else if (guess1.compareTo(guess2) > 0) {
-         return guess1;
-      }
-      else {
-         return guess2;
-      }
-   }
-
+	if (s1 > s2) {
+	    return guess1;
+	}
+	else if (s2 > s1) {
+	    return guess2;
+	}
+	else if (guess1.compareTo(guess2) > 0) {
+	    return guess1;
+	}
+	else {
+	     return guess2;
+	}
+    }
 }
