9b22a99c81779d733018d91d5fd466f2003d8c68
Jon Cooper <joncoop@users.noreply.github.com>
2022-11-17 16:10:07 -0500

    Update TrailTester.java

--- 2010/3. Hiking Trail/TrailTester.java
+++ 2010/3. Hiking Trail/TrailTester.java
@@ -1,12 +1,12 @@
 public class TrailTester {
 	
 	public static void main(String[] args) {
-		int[] elevations1 = {100,150,105,120,90,80,50,75,75,70,80,90,100};
+		int[] elevations = new int[] {100, 150, 105, 120, 90, 80, 50, 75, 75, 70, 80, 90, 100};
 		
-		Trail t = new Trail(elevations1);
+		Trail t = new Trail(elevations);
 
-		System.out.println( t.isLevelTrailSegment(7,10) );
-		System.out.println( t.isLevelTrailSegment(2,12) );
+		System.out.println( t.isLevelTrailSegment(7, 10) );
+		System.out.println( t.isLevelTrailSegment(2, 12) );
 
 		System.out.println( t.isDifficult() );
 	}
