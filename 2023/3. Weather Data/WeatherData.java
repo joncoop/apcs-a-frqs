4ec224b2f16d6e0b9c11cf06ef6f7c40e4f0321c
Jon Cooper <joncoop@users.noreply.github.com>
2023-05-08 09:09:02 -0400

    Update WeatherData.java

--- 2023/3. Weather Data/WeatherData.java
+++ 2023/3. Weather Data/WeatherData.java
@@ -4,29 +4,35 @@ public class WeatherData {
     private ArrayList<Double> temperatures;
 
     public void cleanData(double lower, double upper) {
-        for (int i = temperatures.size() - 1; i >= 0; i--) {
+        int i = 0;
+        
+        while (i < temperatures.size()) {
             double temp = temperatures.get(i);
             boolean outOfRange = temp < lower || temp > upper;
 
             if (outOfRange) {
                 temperatures.remove(i);
             }
+            else {
+                i++;
+            }
         }
     }
 
     public int longestHeatWave(int threshold) {
         int longestStreak = 0;
-        int currentStreak = 0;
+        int currentStreak = 1;
 
-        for (int i = 0; i < temperatures.size(); i++) {
-            double temp = temperatures.get(i);
+        for (int i = 1; i < temperatures.size(); i++) {
+            boolean yesterdayWasHot = temperatures.get(i - 1) > threshold;
+            boolean todayIsHot = temperatures.get(i) > threshold;
 
-            if (temp > threshold) {
+            if (yesterdayWasHot && todayIsHot) {
                 currentStreak++;
                 longestStreak = Math.max(currentStreak, longestStreak);
             }
             else {
-                currentStreak = 0;
+                currentStreak = 1;
             }
         }
 
